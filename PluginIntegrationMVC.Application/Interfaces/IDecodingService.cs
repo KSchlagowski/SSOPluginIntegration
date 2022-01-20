d6c64b85049477819f4f1791917ace7e0d73c7b2
KSchlagowski <kamilschlagowski@gmail.com>
2022-01-20 23:38:42 +0100

    Delete junk code

--- PluginIntegrationMVC.Application/Interfaces/IDecodingService.cs
+++ PluginIntegrationMVC.Application/Interfaces/IDecodingService.cs
@@ -8,7 +8,6 @@ namespace PluginIntegrationMVC.Application.Interfaces
 {
     public interface IDecodingService
     {
-        //string EncodeStringToHex(String input, System.Text.Encoding encoding);
         string EncodeStringToHex(String input);
         string DecodeHexToString(String hexInput);
     }
